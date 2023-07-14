3cce63bb972f2e66608ba965cdd3faef3696b039
Aswin-PK <aswinpk7737@gmail.com>
2023-07-15 01:47:31 +0530

    Added transition effects and changed some styles

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,5 @@
 function scrollfunction(){
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    if(document.body.scrollTop > 350 || document.documentElement.scrollTop > 200){
         document.getElementById("navbar").style.backgroundColor = '#111';
     }
     else{
@@ -7,6 +7,15 @@ function scrollfunction(){
     }
 }
 
-window.onscroll = () =>{
+window.onscroll = (e) =>{
+    // console.log(e);
     scrollfunction();
 }
+
+
+// function searchMovie(){
+//     searchBtn = document.getElementById('search')
+//     searchBtn.addEventListener('focus', ()=>{
+//         searchBtn.
+//     })
+// }
